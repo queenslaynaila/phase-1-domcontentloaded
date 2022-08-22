024eb665c9ac11689b7b16717c71d907e91aa343
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-22 12:31:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const text = document.querySelector("#text");
+  text.textContent = "This is really cool!";
+});
+
+document.getElementsByTagName("p");
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
